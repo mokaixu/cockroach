579599e992f89ed212821ae25d609479da2547f3
Aditya Maru <adityamaru@gmail.com>
2020-11-21 12:04:44 -0500

    [WIP] importccl,sql: support importing non-public schemas from pgdump
    Previously, a PGDUMP import did not support any non-public schema
    statements. Now that CRDB has user defined schemas, bundle format
    IMPORTs need to be taught how to parse, create and cleanup schema
    related PGDUMP operations.
    
    Note this PR only adds support for `CREATE SCHEMA` and usage of the
    schema in `CREATE TABLE/SEQUENCE` PGDUMP statements. `ALTER SCHEMA`
    statements are ignored and support might be added in a follow up.
    
    Release note (sql change): IMPORT PGDUMP can now import dump files with
    non-public schemas.

---
+++ pkg/ccl/importccl/testdata/pgdump/schema.sql
@@ -0,0 +1,141 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1
+-- Dumped by pg_dump version 13.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: bar; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA bar;
+
+
+ALTER SCHEMA bar OWNER TO postgres;
+
+--
+-- Name: baz; Type: SCHEMA; Schema: -; Owner: adityamaru
+--
+
+CREATE SCHEMA baz;
+
+
+ALTER SCHEMA baz OWNER TO adityamaru;
+
+--
+-- Name: foo; Type: SCHEMA; Schema: -; Owner: adityamaru
+--
+
+CREATE SCHEMA foo;
+
+
+ALTER SCHEMA foo OWNER TO adityamaru;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: test; Type: TABLE; Schema: bar; Owner: adityamaru
+--
+
+CREATE TABLE bar.test (
+    id integer,
+    name character varying
+);
+
+
+ALTER TABLE bar.test OWNER TO adityamaru;
+
+--
+-- Name: test; Type: TABLE; Schema: baz; Owner: adityamaru
+--
+
+CREATE TABLE baz.test (
+    id integer,
+    name character varying
+);
+
+
+ALTER TABLE baz.test OWNER TO adityamaru;
+
+--
+-- Name: test; Type: TABLE; Schema: foo; Owner: adityamaru
+--
+
+CREATE TABLE foo.test (
+    id integer,
+    name character varying
+);
+
+
+ALTER TABLE foo.test OWNER TO adityamaru;
+
+--
+-- Name: test; Type: TABLE; Schema: public; Owner: adityamaru
+--
+
+CREATE TABLE public.test (
+    id integer,
+    name character varying
+);
+
+
+ALTER TABLE public.test OWNER TO adityamaru;
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: bar; Owner: adityamaru
+--
+
+COPY bar.test (id, name) FROM stdin;
+1	abc
+2	def
+\.
+
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: baz; Owner: adityamaru
+--
+
+COPY baz.test (id, name) FROM stdin;
+1	abc
+2	def
+\.
+
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: foo; Owner: adityamaru
+--
+
+COPY foo.test (id, name) FROM stdin;
+1	abc
+2	def
+\.
+
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: public; Owner: adityamaru
+--
+
+COPY public.test (id, name) FROM stdin;
+1	abc
+2	def
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
